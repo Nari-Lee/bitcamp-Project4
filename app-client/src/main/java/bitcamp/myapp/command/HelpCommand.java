8d658a841c253c9648b2b5657586170a71f70511
tkddn0321 <godsknight58@naver.com>
2024-08-01 19:13:09 +0900

    0801

--- app-client/src/main/java/bitcamp/myapp/command/HelpCommand.java
+++ app-client/src/main/java/bitcamp/myapp/command/HelpCommand.java
@@ -7,5 +7,6 @@ public class HelpCommand implements Command {
   public void execute(String menuName) {
     System.out.println("베스킨라빈스31 게임 도움말");
     System.out.println("1부터 3까지의 숫자를 번갈아 가며 말하다가 31을 말하는 사람이 지는 게임입니다.");
+    System.out.println("중복된 숫자 허용 모드는 중복 숫자를 입력할 수 없는 배스킨라빈스 게임입니다!\n예) 플레이어 : 2\n    컴퓨터   : 2\n");
   }
 }
